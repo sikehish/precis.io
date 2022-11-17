badec16a64c94f2a110a9c7cdda13779669dcf71
Hisham <hisham0502@gmail.com>
2022-11-17 23:10:57 +0530

    aws config

--- form/views.py
+++ form/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Resume 
 from django.contrib.auth.models import User
-# from django.http import HttpResponse
+# from django.http import HttpResponseNotFound
 
 # Create your views here.
 
